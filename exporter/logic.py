4537d543fd321f342b46537d928cd65e2e95e55e
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-10-12 19:41:18 +0500

    virtual bones

--- exporter/logic.py
+++ exporter/logic.py
@@ -65,176 +65,183 @@ class UEFormatExport:
         collisions: list[importer.classes.ConvexCollision] = []
         skeleton: importer.classes.UEModelSkeleton | None = None
 
-        
         for obj in bpy.data.objects:
-            if obj.type == "MESH":
-                mesh: Mesh = cast(Mesh, obj.data)
-                
-                bm = bmesh.new()
-                bm.from_mesh(mesh)
-                bpy.ops.object.mode_set(mode="EDIT")
-                bmesh.ops.triangulate(bm, faces=bm.faces)
-                bpy.ops.object.mode_set(mode="OBJECT")
-                bm.to_mesh(mesh)
-                bm.free()
-                
-                verts = [v for v in mesh.vertices]
-                ue_verts = np.array([v.co for v in verts], dtype=np.float32)
-                ue_indices = np.array([list(poly.vertices) for poly in mesh.polygons], dtype=np.int32)
-                
-                if obj.display_type != "WIRE":
-                    lod = importer.classes.UEModelLOD(obj.name)
-                        
-                    lod.vertices = ue_verts
-                    lod.indices = ue_indices
-                    lod.normals = np.array([v.normal.to_4d().wxyz for v in verts], dtype=np.float32)
-                    if mesh.uv_layers:
-                        mesh.calc_tangents(uvmap=mesh.uv_layers[0].name)
-                        lod.tangents = np.array([loop.tangent for loop in mesh.loops])
+            with bpy.context.temp_override(selected_objects=[obj]):
+                if obj.type == "MESH":
+                    mesh: Mesh = cast(Mesh, obj.data)
                     
-                    lod.weights = []
-                    armature_of_this_obj: Armature = None
-                    for modifier in obj.modifiers:
-                        if isinstance(modifier, ArmatureModifier):
-                            armature_of_this_obj = modifier.object.data
-                            break
-
-                    for vgroup in obj.vertex_groups:
-                        for vert in verts:
-                            for group in vert.groups:
-                                if group.group == vgroup.index:
-                                    weight = importer.classes.Weight(0, 0, 0.0)
-                                    
-                                    weight.bone_index = armature_of_this_obj.bones.find(vgroup.name)
-                                    weight.vertex_index = vert.index
-                                    weight.weight = group.weight
-                                    
-                                    lod.weights.append(weight)
-                                    break
-
-                    lod.morphs = []
-                    if mesh.shape_keys:
-                        for key in mesh.shape_keys.key_blocks:
-                            key: ShapeKey
-                            morph = importer.classes.MorphTarget(key.name, deltas=[])
-                            morphTargetNormals = np.array(key.normals_vertex_get()).reshape((-1, 3))
-                            for idx in range(len(key.data)):
-                                delta = importer.classes.MorphTargetData([], (0, 0, 0), idx)
-                                
-                                keyPoint: ShapeKeyPoint = key.data[idx]
-                                delta.position = list(keyPoint.co.to_3d().to_tuple())
-                                delta.normals = morphTargetNormals[idx]
-                                
-                                morph.deltas.append(delta)
+                    bm = bmesh.new()
+                    bm.from_mesh(mesh)
+                    bpy.ops.object.mode_set(mode="EDIT")
+                    bmesh.ops.triangulate(bm, faces=bm.faces)
+                    bpy.ops.object.mode_set(mode="OBJECT")
+                    bm.to_mesh(mesh)
+                    bm.free()
+                    
+                    verts = [v for v in mesh.vertices]
+                    ue_verts = np.array([v.co for v in verts], dtype=np.float32)
+                    ue_indices = np.array([list(poly.vertices) for poly in mesh.polygons], dtype=np.int32)
+                    
+                    if obj.display_type != "WIRE":
+                        lod = importer.classes.UEModelLOD(obj.name)
                             
-                            lod.morphs.append(morph)
-
-                    
-                    lod.colors = []
-                    for color_attr in mesh.color_attributes:
-                        color_attr = cast(ByteColorAttribute, color_attr)
+                        lod.vertices = ue_verts
+                        lod.indices = ue_indices
+                        lod.normals = np.array([v.normal.to_4d().wxyz for v in verts], dtype=np.float32)
+                        if mesh.uv_layers:
+                            mesh.calc_tangents(uvmap=mesh.uv_layers[0].name)
+                            lod.tangents = np.array([loop.tangent for loop in mesh.loops])
                         
-                        vcolor = importer.classes.VertexColor(color_attr.name, np.array([]))
-                        vcolor.data = np.array([list(c.color) for c in color_attr.data], dtype=np.float32)
-                        
-                        lod.colors.append(vcolor)
-
-                    
-                    # TODO: fix uvs
-                    lod.uvs = []
-                    for uv_layer in mesh.uv_layers:
-                        lod_uv = []
-                        for uv in uv_layer.uv:
-                            lod_uv.append(np.array(uv.vector.to_tuple(), dtype=np.float32))
-                        lod.uvs.append(np.array(lod_uv))
-
-
-                    lod.materials = []
-                    mat2Poly: dict[int, list[int]] = {}
-                    for poly in mesh.polygons:
-                        mat2Poly[poly.material_index] = mat2Poly.get(poly.material_index, []) + [poly.index]
-                    for i, material in enumerate(mesh.materials):
-                        material: Material
-                        
-                        # this happens if the material isn't used by any vertex
-                        try:
-                            polys = mat2Poly[i]
-                        except KeyError:
-                            continue
-
-                        polys.sort()
-                        start = 0
-                        end = 1
-                        while end < len(polys):
-                            # expand  the window [start, end] as long as these are consecutive faces
-                            while end < len(polys) and polys[end] - polys[start] == end - start:
-                                end += 1
-
-                            lod_mat = importer.classes.Material(material.name, 0, 0)
-                            lod_mat.first_index = polys[start]
-                            lod_mat.num_faces = end - start
-                            lod.materials.append(lod_mat)
-                            
-                            start = end
-                    
-                    lods.append(lod)
-
-                else:
-                    collision = importer.classes.ConvexCollision(obj.name, ue_verts, ue_indices)
-                    collisions.append(collision)
-
-            elif obj.type == "ARMATURE":
-                armature = cast(Armature, obj.data)
-                skeleton = importer.classes.UEModelSkeleton()
-
-                bpy.ops.object.mode_set(mode="EDIT")
-                edit_bones = armature.bones
-                for edit_bone in edit_bones:
-                    bone = importer.classes.Bone(edit_bone.name, -1, [], (0.0, 0.0, 0.0, 0.0))
-                    translation, rotation, _ = edit_bone.matrix.to_4x4().decompose()
-                    bone.position = list(translation.to_tuple())
-                    bone.rotation = (rotation.w, rotation.x, rotation.y, rotation.z)
-
-                    if edit_bone.parent:
-                        bone.parent_index = armature.bones.find(edit_bone.parent.name)
-                    
-                    skeleton.bones.append(bone)
-                    
-                bpy.ops.object.mode_set(mode="OBJECT")
-
-                # TODO: sockets
-
-                if armature.collections.find("Virtual Bones") != -1:
-
-                    virtual_bone_collection: BoneCollection = armature.collections[armature.collections.find("Virtual Bones")]
-                    for bone in virtual_bone_collection.bones:
-                        bpy.ops.object.mode_set(mode="EDIT")
-                        edit_bones = armature.edit_bones
-                        lod_vbone = importer.classes.VirtualBone("", "", bone.name)
-
-                        for source_bone in edit_bones:
-                            if source_bone.tail == bone.head and source_bone.head == bone.tail:
-                                lod_vbone.source_name = source_bone.name
+                        lod.weights = []
+                        armature_of_this_obj: Armature = None
+                        for modifier in obj.modifiers:
+                            if isinstance(modifier, ArmatureModifier):
+                                armature_of_this_obj = modifier.object.data
                                 break
+
+                        for vgroup in obj.vertex_groups:
+                            for vert in verts:
+                                for group in vert.groups:
+                                    if group.group == vgroup.index:
+                                        weight = importer.classes.Weight(0, 0, 0.0)
+                                        
+                                        weight.bone_index = armature_of_this_obj.bones.find(vgroup.name)
+                                        weight.vertex_index = vert.index
+                                        weight.weight = group.weight
+                                        
+                                        lod.weights.append(weight)
+                                        break
+
+                        lod.morphs = []
+                        if mesh.shape_keys:
+                            for key in mesh.shape_keys.key_blocks:
+                                key: ShapeKey
+                                morph = importer.classes.MorphTarget(key.name, deltas=[])
+                                morphTargetNormals = np.array(key.normals_vertex_get()).reshape((-1, 3))
+                                for idx in range(len(key.data)):
+                                    delta = importer.classes.MorphTargetData([], (0, 0, 0), idx)
+                                    
+                                    keyPoint: ShapeKeyPoint = key.data[idx]
+                                    delta.position = list(keyPoint.co.to_3d().to_tuple())
+                                    delta.normals = morphTargetNormals[idx]
+                                    
+                                    morph.deltas.append(delta)
+                                
+                                lod.morphs.append(morph)
+
                         
-                        if lod_vbone.source_name == "":
-                            continue
+                        lod.colors = []
+                        for color_attr in mesh.color_attributes:
+                            color_attr = cast(ByteColorAttribute, color_attr)
+                            
+                            vcolor = importer.classes.VertexColor(color_attr.name, np.array([]))
+                            vcolor.data = np.array([list(c.color) for c in color_attr.data], dtype=np.float32)
+                            
+                            lod.colors.append(vcolor)
+
                         
-                        bpy.ops.object.mode_set(mode="POSE")
-                        pose_bone: PoseBone | None = obj.pose.bones.get(bone.name)
-                        if pose_bone is None:
-                            continue
+                        # TODO: fix uvs
+                        lod.uvs = []
+                        for uv_layer in mesh.uv_layers:
+                            lod_uv = []
+                            for uv in uv_layer.uv:
+                                lod_uv.append(np.array(uv.vector.to_tuple(), dtype=np.float32))
+                            lod.uvs.append(np.array(lod_uv))
+
+
+                        lod.materials = []
+                        mat2Poly: dict[int, list[int]] = {}
+                        for poly in mesh.polygons:
+                            mat2Poly[poly.material_index] = mat2Poly.get(poly.material_index, []) + [poly.index]
+                        for i, material in enumerate(mesh.materials):
+                            material: Material
+                            
+                            # this happens if the material isn't used by any vertex
+                            try:
+                                polys = mat2Poly[i]
+                            except KeyError:
+                                continue
+
+                            polys.sort()
+                            start = 0
+                            end = 1
+                            while end < len(polys):
+                                # expand  the window [start, end] as long as these are consecutive faces
+                                while end < len(polys) and polys[end] - polys[start] == end - start:
+                                    end += 1
+
+                                lod_mat = importer.classes.Material(material.name, 0, 0)
+                                lod_mat.first_index = polys[start]
+                                lod_mat.num_faces = end - start
+                                lod.materials.append(lod_mat)
+                                
+                                start = end
                         
-                        if pose_bone.constraints.find("IK") != -1:
-                            constraint = pose_bone.constraints[pose_bone.constraints.find("IK")]
+                        lods.append(lod)
+
+                    else:
+                        collision = importer.classes.ConvexCollision(obj.name, ue_verts, ue_indices)
+                        collisions.append(collision)
+
+                elif obj.type == "ARMATURE":
+                    armature = cast(Armature, obj.data)
+                    skeleton = importer.classes.UEModelSkeleton()
+
+                    armature_bones = armature.bones
+                    for a_bone in armature_bones:
+                        bone = importer.classes.Bone(a_bone.name, -1, [], (0.0, 0.0, 0.0, 0.0))
+                        translation, rotation, _ = a_bone.matrix.to_4x4().decompose()
+                        bone.position = list(translation.to_tuple())
+                        bone.rotation = (rotation.x, rotation.y, rotation.z, rotation.w)
+
+                        if a_bone.parent:
+                            bone.parent_index = armature.bones.find(a_bone.parent.name)
+                        
+                        skeleton.bones.append(bone)
+
+                    # TODO: sockets
+
+                    if armature.collections.find("Virtual Bones") != -1:
+
+                        virtual_bone_collection: BoneCollection = armature.collections["Virtual Bones"]
+
+                        for bone in virtual_bone_collection.bones:
+                            armature_bones = armature.bones
+                            lod_vbone = importer.classes.VirtualBone("", "", bone.name)
+
+                            for source_bone in armature_bones:
+                                if source_bone.tail == bone.head and source_bone.head == bone.tail:
+                                    lod_vbone.source_name = source_bone.name
+                                    break
+                            
+                            if lod_vbone.source_name == "":
+                                continue
+                            
+                            bpy.ops.object.mode_set(mode="POSE")
+                            pose_bone: PoseBone | None = obj.pose.bones[bone.name]
+                            if pose_bone is None:
+                                continue
+                            
+                            if pose_bone.constraints.find("IK") == -1:
+                                continue
+
+                            constraint = pose_bone.constraints["IK"]
                             constraint = cast(KinematicConstraint, constraint)
                             lod_vbone.target_name = constraint.subtarget
 
-                        skeleton.virtual_bones.append(lod_vbone)
-                    
-                    bpy.ops.object.mode_set(mode="OBJECT")
+                            skeleton.virtual_bones.append(lod_vbone)
+
+                            idx = -1
+                            for i, b in enumerate(skeleton.bones):
+                                if b.name == lod_vbone.virtual_name:
+                                    idx = i
+                                    break
+                            if idx != -1:
+                                skeleton.bones.pop(idx)
+                        
+                        bpy.ops.object.mode_set(mode="OBJECT")
+                
             
-        
         uemodel = importer.classes.UEModel()
         if lods and len(lods) != 0:
             uemodel.lods = lods
